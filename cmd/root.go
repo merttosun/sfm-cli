8063a63694946f3fc90a4f342afc0bf2382d5c67
mert.tosun <mert.tosun@trendyol.com>
2022-08-23 13:58:37 +0300

    version added

--- cmd/root.go
+++ cmd/root.go
@@ -13,8 +13,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "sfm-cli",
-	Short: "A brief description of your application",
+	Use:     "sfm-cli",
+	Version: "0.0.1",
+	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
